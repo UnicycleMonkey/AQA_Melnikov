22fa41fb1b22ea9879ece1163a5e0aba9666fcb9
Melnikov Vadim <melnikov_regmail@tut.by>
2025-03-05 14:52:49 +0300

    Повышена атомизация методов, зависимость вывода от консоли вынесена в отдельные методы

--- src/Main.java
+++ src/Main.java
@@ -6,39 +6,24 @@ public class Main {
         checkSumSign();   //02
         printColor();   //03
         compareNumbers();   //04
-
-        int a05=2, b05=10;  //05
-        String possibleNot05=sumLaysBetween10And20(a05,b05)?"":" не";
-        System.out.println("Сумма "+a05+" и "+b05+possibleNot05+" лежит в полуинтервале (10;20]");
-
-        printIsNumberPositive(generateRandomInt(-3,3)); //06
-
-        int num07=generateRandomInt(-3,3);  //07
-        String possibleNot07=isNumberNegative(num07)?"":" не";
-        System.out.println("Число "+num07+possibleNot07+" является отрицательным");
-
+        printSumLaysBetween(2,10);  //05
+        printIsNumberPositive (generateRandomInt(-3,3)); //06
+        printIsNumberNegative (generateRandomInt(-3,3));  //07
         printStringNTimes("Cookies!",3); //08
-
-        int year09 = 2100;  //09
-        String possibleNot09=(isYearLeap(year09))?"":" не";
-        System.out.println("Год "+year09+possibleNot09+" является високосным");
-
-        reverse1And0InArray(); //10
-        fillArray1to100();    //11
-        doubleNumbersLess6InArray();    //12
-        fillDiagonalsWith1(); //13
-
+        printIsYearLeap(2100); //09
+        manageArrayT10(); //10
+        manageArrayT11();    //11
+        manageArrayT12();    //12
+        manageSquareArrayT13(); //13
         int[] array14 = createAndInitializeArray(8, 69);    //14
-        System.out.println("Массив:\n"+ Arrays.toString(array14));
-
-
+        printIntArray(array14,true);
     }
-
+    
 /* 1.Создайте метод printThreeWords(), который при вызове должен отпечатать
      в столбец три слова: Orange, Banana, Apple.
 */
     private static void printThreeWords() {
-        System.out.println("Orange\nBanana\nApple");
+        printlnOut("Orange\nBanana\nApple");
     }
 
 /* 2.Создайте метод checkSumSign(), в теле которого объявите две int переменные
@@ -49,12 +34,12 @@ public class Main {
     private static void  checkSumSign(){
         int a = generateRandomInt(-10,10);
         int b = generateRandomInt(-6,2);
-        System.out.println("Values are: "+a+", "+b);
+        printlnOut("Values are: "+a+", "+b);
         if (isNumberPositive(a+b)) {
-            System.out.println("Сумма положительная");
+            printlnOut("Сумма положительная");
         }
         else{
-            System.out.println("Сумма отрицательная");
+            printlnOut("Сумма отрицательная");
         }
     }
 
@@ -66,7 +51,7 @@ public class Main {
     private static void printColor(){
         String resultColor;
         int randomValue = generateRandomInt(-100,200);
-        System.out.println("Value: "+randomValue);
+        printlnOut("Value: "+randomValue);
         if (randomValue<=0){
             resultColor="Красный";
         }
@@ -76,7 +61,7 @@ public class Main {
         else{
             resultColor="Зеленый";
         }
-        System.out.println(resultColor);
+        printlnOut(resultColor);
     }
 
 /* 4.Создайте метод compareNumbers(), в теле которого объявите две int переменные
@@ -86,9 +71,9 @@ public class Main {
     private static void compareNumbers(){
         int a = generateRandomInt(0,10);
         int b = generateRandomInt(0,10);
-        System.out.println("Values are: "+a+", "+b);
+        printlnOut("Values are: "+a+", "+b);
         String result= ((a >= b)? "а >= b": "а < b");
-        System.out.println(result);
+        printlnOut(result);
     }
 
 /* 5.Напишите метод, принимающий на вход два целых числа и проверяющий, что их сумма
@@ -105,7 +90,7 @@ public class Main {
  */
     private static void printIsNumberPositive(int num){
         String result = isNumberPositive(num)? " положительное": " отрицательное";
-        System.out.println("Число "+num+result);
+        printlnOut("Число "+num+result);
     }
 
 /* 7.Напишите метод, которому в качестве параметра передается целое число. Метод
@@ -121,7 +106,7 @@ public class Main {
     private static void printStringNTimes(String str, int n){
         if(n>0) {
             for (int i = 0; i<n; i++) {
-                System.out.println(str);
+                printlnOut(str);
             }
         }
     }
@@ -144,70 +129,90 @@ public class Main {
         return result;
     }
 
+    private static void printIsYearLeap(int year) {
+        String possibleNot09=(isYearLeap(year))?"":" не";
+        printlnOut("Год "+year+possibleNot09+" является високосным");
+    }
 /* 10.Задать целочисленный массив, состоящий из элементов 0 и 1. Например:
      [ [1, 1, 0, 0,1, 0, 1, 1, 0, 0 ]. С помощью цикла и условия заменить 0 на 1,1 на 0;*/
-    private static void reverse1And0InArray(){
+    private static void manageArrayT10(){
         final int SIZE = 8;
         int[] array = new int[SIZE];
         for (int i=0;i<array.length;i++){
             array[i]=generateRandomInt(0,1);
         }
-        System.out.println("Исходный массив:\n"+ Arrays.toString(array));
-        for (int i=0;i<array.length;i++){
-            array[i]=(array[i]==0)? 1 :0;
-        }
-        System.out.println("Преобразованный массив:\n"+ Arrays.toString(array));
+        printIntArray(array,false);
+        reverse1And0InArray(array);
+        printIntArray(array,true);
     }
 
-/* 11.Задать пустой целочисленный массив длиной 100. С помощью цикла заполнить его значениями 1 2 3 4 5 6 7 8 ... 100;*/
-    private static void fillArray1to100(){
+    private static void reverse1And0InArray(int[] array) {
+        for (int i = 0; i< array.length; i++){
+            array[i] = (array[i]==0)? 1 :0;
+        }
+    }
+
+    /* 11.Задать пустой целочисленный массив длиной 100. С помощью цикла заполнить его значениями 1 2 3 4 5 6 7 8 ... 100;*/
+    private static void manageArrayT11(){
         final int SIZE = 100;
         int[] array = new int[SIZE];
-        for (int i=0;i<array.length;i++){
-            array[i]=i+1;
-        }
-        System.out.println("Массив:\n"+ Arrays.toString(array));
+        fillArrayFrom1ToLen(array);
+        printIntArray(array,true);
     }
 
-/* 12.Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1] пройти по нему циклом, и числа меньшие 6 умножить на 2;*/
-    private static void doubleNumbersLess6InArray(){
+    private static void fillArrayFrom1ToLen(int[] array) {
+        for (int i = 0; i< array.length; i++){
+            array[i]=i+1;
+        }
+    }
+
+    /* 12.Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1] пройти по нему циклом, и числа меньшие 6 умножить на 2;*/
+    private static void manageArrayT12(){
         int[] array = { 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-        System.out.println("Исходный массив:\n"+ Arrays.toString(array));
-        for (int i=0;i<array.length;i++){
+        printIntArray(array,false);
+        doubleNumbersLess6InArray(array);
+        printIntArray(array,true);
+    }
+
+    private static void doubleNumbersLess6InArray(int[] array) {
+        for (int i = 0; i< array.length; i++){
             if(array[i]<6){
                 array[i]*=2;
             }
         }
-        System.out.println("Массив:\n"+ Arrays.toString(array));
     }
 
-/* 13.Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое),
-    и с помощью цикла(-ов) заполнить его диагональные элементы единицами (можно только одну из диагоналей,
-    если обе сложно). Определить элементы одной из диагоналей можно по следующему принципу: индексы таких
-    элементов равны, то есть [0][0], [1][1], [2][2], ..., [п][п];*/
-    private static void fillDiagonalsWith1(){
+    /* 13.Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое),
+        и с помощью цикла(-ов) заполнить его диагональные элементы единицами (можно только одну из диагоналей,
+        если обе сложно). Определить элементы одной из диагоналей можно по следующему принципу: индексы таких
+        элементов равны, то есть [0][0], [1][1], [2][2], ..., [п][п];*/
+    private static void manageSquareArrayT13(){
         final int SIZE = 6;
         int[][] square = new int[SIZE][SIZE];
-        for (int i=0; i<SIZE;i++){
-            for (int j=0; j<SIZE; j++){
-                if((i==j) || (i+j==SIZE-1)){
+        fillDiagonalsWith1(square);
+        for(int i=0; i<SIZE; i++) {
+            for (int j=0; j<SIZE; j++) {
+                printOut(square[i][j]+"\t");
+            }
+            printlnOut("");
+        }
+    }
+
+    private static void fillDiagonalsWith1(int[][] square) {
+        for (int i = 0; i< square.length; i++){
+            for (int j = 0; j< square[0].length; j++){
+                if((i==j) || (i+j== square.length -1)){
                     square[i][j]=1;
                 } else {
                     square[i][j]=0;
                 }
             }
         }
-        for(int i=0; i<SIZE; i++) {
-            for (int j=0; j<SIZE; j++) {
-                System.out.print(square[i][j]+"\t");
-            }
-            System.out.println();
-        }
     }
 
-/* 14.Написать метод, принимающий на вход два аргумента: len и initialValue, и возвращающий одномерный
-    массив типа int длиной len, каждая ячейка которого равна initialValue.
-*/
+    /* 14.Написать метод, принимающий на вход два аргумента: len и initialValue, и возвращающий одномерный
+        массив типа int длиной len, каждая ячейка которого равна initialValue.
+    */
     private static int[] createAndInitializeArray(int len, int initialValue){
         if(len<=0){
             return null;
@@ -216,12 +221,38 @@ public class Main {
         Arrays.fill(result,initialValue);
         return result;
     }
+    
+    private static void printSumLaysBetween(int a, int b) {
+        String possibleNot05=sumLaysBetween10And20(a,b)?"":" не";
+        printlnOut("Сумма "+a+" и "+b+possibleNot05+" лежит в полуинтервале (10;20]");
+    }
 
+    private static void printIsNumberNegative(int num) {
+        String possibleNot07=isNumberNegative(num)?"":" не";
+        printlnOut("Число "+num+possibleNot07+" является отрицательным");
+    }
+
+    private static void printlnOut(String str) {
+        System.out.println(str);
+    }
+    
+    private static void printOut(String str) {
+        System.out.printf(str);
+    }
+    
     private static boolean isNumberPositive(int arg){
         return arg >= 0;
     }
 
     private static int generateRandomInt(int min, int max) {
         return (int)(Math.random()*(max-min+1)) + min;
+    }
+
+    private static void printIntArray(int[] array,boolean wasModified) {
+        if (wasModified) {
+            printlnOut("Итоговый массив:\t" + Arrays.toString(array));
+        }else {
+            printlnOut("Исходный массив:\t" + Arrays.toString(array));
+        }
     }
 }
