61197d93f8f889e7d12f2daf856e110aad881d3c
Melnikov Vadim <melnikov_regmail@tut.by>
2025-03-24 04:28:23 +0300

    ДЗ#8. Готово

--- AQA_Melnikov/src/main/java/AQA08/pages/MTSMainPage.java
+++ AQA_Melnikov/src/main/java/AQA08/pages/MTSMainPage.java
@@ -1,19 +1,45 @@
 package AQA08.pages;
 
+import org.openqa.selenium.By;
+
 public class MTSMainPage {
-    private final String acceptCookieButtonXPath = "//*[@id='cookie-agree']";
-    private final String payContinueButtonXPath = "//*[@id=\"pay-connection\"]/button";
-    private final String payPhoneInputXPath = "//*[@id=\"connection-phone\"]";
+    private String acceptCookieButtonXPath = "//*[@id='cookie-agree']";
+    private String paymentSystemsLogoUlXPath = "//*[@class='pay__partners']/ul/";
+    private String aboutServiceLinkXPath = "//*[@id='pay-section']//a[text()='Подробнее о сервисе']";
+    private String inputPhoneXPath = "//*[@id='connection-phone']";
+    private String inputPaymentXPath = "//*[@id='connection-sum']";
+    private String proceedPaymentButtonXPath = "//*[@id='pay-connection']/button[@class='button button__default ']";
+    private String iframePaymentConfirmationXPath = "//div[@class='bepaid-app']/div/iframe";
 
-    public String getAcceptCookieButtonXPath(){
-        return acceptCookieButtonXPath;
+    public By getAcceptCookieButton(){
+        return By.xpath(acceptCookieButtonXPath);
     }
 
-    public String getPayContinueButtonXPath() {
-        return payContinueButtonXPath;
+    public By getPaymentSystemsLogoUl(){
+        return By.xpath(paymentSystemsLogoUlXPath);
     }
 
-    public String getPayPhoneInputXPath() {
-        return payPhoneInputXPath;
+    public By getPaymentSystemLogo(int i){
+        return By.xpath(paymentSystemsLogoUlXPath+"li["+i+"]");
+    }
+
+    public By getAboutServiceLink() {
+        return By.xpath(aboutServiceLinkXPath);
+    }
+
+    public By getInputPhone() {
+        return By.xpath(inputPhoneXPath);
+    }
+
+    public By getInputPayment(){
+        return By.xpath(inputPaymentXPath);
+    }
+
+    public By getProceedPaymentButton(){
+        return By.xpath(proceedPaymentButtonXPath);
+    }
+
+    public By getIframePaymentConfirmation(){
+        return By.xpath(iframePaymentConfirmationXPath);
     }
 }
--- AQA_Melnikov/src/test/java/MTSSiteTest.java
+++ AQA_Melnikov/src/test/java/MTSSiteTest.java
@@ -1,6 +1,7 @@
 
 import AQA08.pages.MTSMainPage;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import jdk.jfr.Description;
 import org.junit.jupiter.api.*;
 import org.testng.Assert;
 import org.openqa.selenium.*;
@@ -13,61 +14,111 @@ public class MTSSiteTest {
 
     public MTSSiteTest(){
         mainPage = new MTSMainPage();
-    }
-
-    @BeforeAll
-    static void setupClass() {
         WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
     void setupTest() {
+        System.out.println("Перед каждым тестом");
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
         driver.get(URL);
         explicitWait(1000);
-        WebElement acceptCookieButton = driver.findElement(By.xpath(mainPage.getAcceptCookieButtonXPath()));
-        acceptCookieButton.click();
-        explicitWait(1000);
+        handleCookie();
     }
 
     @AfterEach
     void closeBrowserWindow() {
+        System.out.println("Закрываю окно");
         driver.quit();
         driver=null;
     }
 
-    @Test
-    void test() {
-        String title = driver.getTitle();
-        Assert.assertTrue(title.contains("МТС – мобильный оператор в Беларуси"));
-    }
 
     @Test
+    @Description("Проверка заголовка оплаты")
     void testPayTitle(){
-        Assert.assertTrue(tryToFind("//h2[contains(text(),'Онлайн пополнение']"));
+        WebElement we = driver.findElement(By.xpath("//div[@class='pay__wrapper']/h2"));
+        String str = we.getText();
+        Assert.assertTrue(we.isDisplayed());
+        Assert.assertEquals(str, "Онлайн пополнение\nбез комиссии");
     }
 
-    private boolean tryToFind(String xPath) {
+
+    @Test
+    @Description("Проверка лого платежных систем")
+    void testPaymentLogos(){
+        System.out.println("тест логотипов");
         boolean result = true;
-        try {
-            WebElement we = driver.findElement(By.xpath(xPath));
-        } catch (NoSuchElementException e) {
-            result =false;
+        for (int i = 1; i <= 5; i++) {
+            result= result && tryToFind(mainPage.getPaymentSystemLogo(i));
         }
-        return result;
+        Assert.assertTrue(result);
+    }
+
+
+    @Test
+    @Description("Ссылка \"Подробнее о сервисе\"")
+    void testAboutLink(){
+        System.out.println("Проверка ссылки \"Подробнее о сервисе\"");
+        WebElement linkAbout = driver.findElement(mainPage.getAboutServiceLink());
+        linkAbout.click();
+        Assert.assertEquals(driver.getTitle(), "Порядок оплаты и безопасность интернет платежей");
     }
 
     @Test
-    void testPaymentLogos(){
+    @Description("Проверка оплаты")
+    void testPayment(){
+        System.out.println("Проверка работоспособности оплаты");
+        final String MOCK_PHONE = "297777777";
+        final String PAYMENT = "1";
 
+        WebElement inputPhone = driver.findElement (mainPage.getInputPhone());
+        WebElement inputPayment = driver.findElement (mainPage.getInputPayment());
+        WebElement proceedButton = driver.findElement (mainPage.getProceedPaymentButton());
+
+        inputPhone.sendKeys(MOCK_PHONE+Keys.ENTER);
+        inputPayment.sendKeys(PAYMENT);
+        proceedButton.click();
+        explicitWait(5000);
+
+        Assert.assertTrue(driver.findElement (mainPage.getIframePaymentConfirmation()).isEnabled());
 
     }
 
     private static void explicitWait(int ms) {
         try{
             Thread.sleep(ms);
+            System.out.println("Жду "+ms+" мс");
         }
         catch (InterruptedException e){
+            System.out.println("Что-то сломалось во время ожидания: "+e.getMessage());
         }
+    }
+
+    private boolean tryToFind(By byXPass) {
+        boolean result = true;
+        System.out.println("Ищу "+byXPass);
+        try {
+            driver.findElement(byXPass);
+        } catch (NoSuchElementException e) {
+            result =false;
+            System.out.println("Не нашел "+byXPass);
+        }
+        return result;
+    }
+
+    private void handleCookie() {
+        try {
+            System.out.println("жду окошко с куками");
+            WebElement acceptCookieButton = driver.findElement(mainPage.getAcceptCookieButton());
+            if(acceptCookieButton.isEnabled()) {
+                acceptCookieButton.click();
+                System.out.println("Тыкнул в 'принять'");
+            }
+        }catch (ElementNotInteractableException e){
+            System.out.println("Окошка не было");
+        }
+        explicitWait(500);
     }
 }
